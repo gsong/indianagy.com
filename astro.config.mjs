11e3013555dbeb9d56b455cb1a071d05da851486
George Song <george@damacy.net>
2023-10-20 18:07:16 -0700

    feat: more home page styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import compress from "astro-compress";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()],
+  integrations: [compress(), purgecss()],
 });
