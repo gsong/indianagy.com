d375470e4035199c464a802d6d646c1ecf863d87
George Song <george@gsong.dev>
2025-01-18 10:25:15 -0800

    feat: add shadcn/ui

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,14 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
