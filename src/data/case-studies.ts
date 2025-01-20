e8f9878ff01863e3ec4601e57c5618758ec6eabd
George Song <george@gsong.dev>
2025-01-19 17:08:44 -0800

    feat: update homepage cards

---
+++ src/data/case-studies.ts
@@ -0,0 +1,9 @@
+import amazonImage from "@/images/home/case-studies/amazon.png";
+import myntzImage from "@/images/home/case-studies/myntz.png";
+import skippyImage from "@/images/home/case-studies/skippy.png";
+
+export const caseStudies = [
+  { label: "Amazon Grocery", href: "#", imgSrc: amazonImage },
+  { label: "Skippy Nut Roll", href: "#", imgSrc: skippyImage },
+  { label: "Myntz! Breathmints", href: "#", imgSrc: myntzImage },
+];
