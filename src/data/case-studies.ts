1f099422f03852177692087794cb9bc5d376ed02
George Song <george@gsong.dev>
2025-01-19 19:56:47 -0800

    feat: detail page

--- src/data/case-studies.ts
+++ src/data/case-studies.ts
@@ -3,7 +3,11 @@ import myntzImage from "@/images/home/case-studies/myntz.png";
 import skippyImage from "@/images/home/case-studies/skippy.png";
 
 export const caseStudies = [
-  { label: "Amazon Grocery", href: "#", imgSrc: amazonImage },
+  {
+    label: "Amazon Grocery",
+    href: "/case-study/amazon-grocery",
+    imgSrc: amazonImage,
+  },
   { label: "Skippy Nut Roll", href: "#", imgSrc: skippyImage },
   { label: "Myntz! Breathmints", href: "#", imgSrc: myntzImage },
 ];
