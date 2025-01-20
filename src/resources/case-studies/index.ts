cc67b7d4d49b206c93f20f5604a3985b957fd54e
George Song <george@gsong.dev>
2025-01-20 11:18:09 -0800

    feat: add myntz!

--- src/resources/case-studies/index.ts
+++ src/resources/case-studies/index.ts
@@ -1,6 +1,7 @@
 import type { DetailMetadata } from "../types";
 
 import amazon from "./amazon-grocery";
+import myntz from "./myntz";
 import skippy from "./skippy";
 
-export const caseStudies: DetailMetadata[] = [amazon, skippy];
+export const caseStudies: DetailMetadata[] = [amazon, skippy, myntz];
---
+++ src/resources/case-studies/myntz/index.tsx
@@ -0,0 +1,46 @@
+import type { DetailMetadata } from "../../types";
+
+import { initGetImage } from "../../utils";
+const images = import.meta.glob("./images/*.{png,jpg}");
+
+const getImage = initGetImage(images);
+
+export const sections = [
+  {
+    label: "Strategy",
+    images: ["strategy-1.png", "strategy-2.png"].map(getImage),
+  },
+  { label: "Photography Art Direction", images: [getImage("photo.png")] },
+  {
+    label: "Packaging",
+    images: ["packaging-1.png", "packaging-2.png"].map(getImage),
+  },
+  { label: "Store Displays", images: [getImage("store.png")] },
+];
+
+const description = () => (
+  <p>
+    When the brand transitioned to cleaner ingredients, I saw a chance to
+    revitalize its visual identity. I modernized the packaging and logo design
+    to reflect this change while supporting the introduction of new flavors.
+    This case study showcases how I helped the brand refresh its image while
+    staying true to its market presence.
+  </p>
+);
+
+export default {
+  label: "Myntz! Breathmint",
+  href: `${import.meta.env.BASE_URL}case-study/myntz`,
+  imgSrc: getImage("card.png"),
+  highlights: [
+    "Brand Strategy",
+    "Photography Art Direction",
+    "Logo Redesign",
+    "Color",
+    "Packaging Redesign",
+    "POS Design",
+  ],
+  sections,
+  description,
+  credit: "Credit: Myntz! on solid blue backgrounds @Julee Ho and team",
+} satisfies DetailMetadata;
