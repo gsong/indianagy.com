60fe72d38353e9bdf1cc461b222413949d9ad453
George Song <george@gsong.dev>
2025-01-23 07:34:26 -0800

    fix: allow all r2 images to be optimized

--- astro.config.ts
+++ astro.config.ts
@@ -16,9 +16,7 @@ export default defineConfig({
     }),
   ],
 
-  image: {
-    domains: ["pub-2b21573a122e4116b29744d5dfaf96d6.r2.dev"],
-  },
+  image: { remotePatterns: [{ protocol: "https", hostname: "*.r2.dev" }] },
 
   server: { host: true },
   vite: {
