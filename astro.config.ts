2ca7e0b760c97d5a43c49d5d2f0d7af7ceecd5ac
George Song <george@gsong.dev>
2025-02-02 11:36:42 -0800

    feat: generate sitemap and robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
@@ -11,6 +11,9 @@ export default defineConfig({
 
   integrations: [
     react(),
+    sitemap({
+      lastmod: new Date(),
+    }),
     tailwind({
       applyBaseStyles: false,
     }),
